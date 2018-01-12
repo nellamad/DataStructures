48b08b8fa6ecb8b709a63f000c02034c1f8bdb44
nellamad <allenqdam@gmail.com>
2018-01-11 23:33:10 -0500

    Included more test input files.
    Included the five_int.txt and zero_int.txt test input files.

--- src/test/java/DataStructures/RedBlackTreeTest.java
+++ src/test/java/DataStructures/RedBlackTreeTest.java
@@ -6,52 +6,60 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RedBlackTreeTest {
-    RedBlackTree tree;
-    static ArrayList<Integer> nodeData = new ArrayList<>();
+    private RedBlackTree tree;
+    static String[] input_file_names = Stream.of("src/test/resources/zero_int.txt",
+            "src/test/resources/five_int.txt",
+            "src/test/resources/hundred_int.txt").toArray(String[]::new);
 
     @BeforeAll
     static void setUpAll() {
 
-        try {
-            // need to specify utf-16 encoding since our test input is generated from python
-            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources/hundred_int.txt"), "UTF-16"));
-            while(in.ready()) {
-                String line = in.readLine().trim();
-                nodeData.add(Integer.parseInt(line));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @BeforeEach
     void setUp() {
-        tree = new RedBlackTree();
-    }
 
+    }
 
     @Test
     void insertAndDelete() {
-        System.out.println("Starting insert test");
-        for (int data : nodeData) {
-            System.out.println("Adding " + data);
-            tree.insert(new RedBlackNode(data));
-            tree.printTree();
-            tree.validate();
-        }
+        ArrayList<Integer> nodeData = new ArrayList<>();
+        for (String file_name : input_file_names) {
+            System.out.println("Starting test with input from: " + file_name);
+            tree = new RedBlackTree();
+            try {
+                // need to specify utf-16 encoding since our test input is generated from python
+                BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file_name), "UTF-16"));
+                while(in.ready()) {
+                    String line = in.readLine().trim();
+                    nodeData.add(Integer.parseInt(line));
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        System.out.println("Starting delete test");
-        for (int data : nodeData) {
-            System.out.println("Deleting " + data);
-            tree.delete(data);
-            tree.printTree();
-            tree.validate();
+            System.out.println("Starting insert test");
+            for (int data : nodeData) {
+                System.out.println("Adding " + data);
+                tree.insert(new RedBlackNode(data));
+                tree.printTree();
+                tree.validate();
+            }
+
+            System.out.println("Starting delete test");
+            for (int data : nodeData) {
+                System.out.println("Deleting " + data);
+                tree.delete(data);
+                tree.printTree();
+                tree.validate();
+            }
         }
     }
 }
